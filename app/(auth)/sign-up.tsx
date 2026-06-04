5731214657202d3eaf2dc61032cb824978de900a
Kailash Kumar <202051093@iiitvadodara.ac.in>
2026-06-04 10:33:51 +0530

    first commit

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const SignUp = () => {
+    return (
+        <View>
+            <Text>SignUp</Text>
+            <Link href="/(auth)/sign-in" className="text-white mt-4 rounded bg-primary p-4">
+                Sign In
+            </Link>
+        </View>
+    )
+}
+
+export default SignUp
---
+++ app/(tabs)/subscriptions/[id].tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link, useLocalSearchParams } from 'expo-router'
+
+const SubscriptionsDetails = () => {
+    const { id } = useLocalSearchParams<{ id: string }>();
+  return (
+    <View>
+      <Text>SubscriptionsDetails : {id}</Text>
+      <Link href="/">Go Back</Link>
+    </View>
+  )
+}
+
+export default SubscriptionsDetails
